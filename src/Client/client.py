13b56a904b1f7804dbb3d6173ecbc335ec659885
nventuri97 <n.venturi3@studenti.unipi.it>
2020-10-08 17:44:47 +0200

    Update

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,7 +1,8 @@
 #Client for COVID-19 application and to evaluate client anonymous trace
 
 from Crypto.Cipher import AES
-from http.client import *
+from http.client import HTTPSConnection
+import json
 
 #Example trace
 trace='a(-)a(-)s(-)v(-)s(+)s(+)a(+)s(-)s(-)'
@@ -11,3 +12,12 @@ key=b'Sixteen byte key'
 cipher=AES.new(key,AES.MODE_EAX)
 
 #Connection with server
+connection=HTTPSConnection('127.0.0.1', 4443, timeout=10)
+if ConnectionError :
+    print("Something went wrong, try later")
+
+#Encrypted trace needed to execute OT
+cipher_text, tag= cipher.encrypt_and_digest(trace)
+data=json.dumps({"encrypt_trace":cipher_text, "len":cipher_text.__len__})
+connection.send(data)
+
--- src/Server/server.py
+++ src/Server/server.py
@@ -1,12 +1,35 @@
 #HTTPS Server for COVID-19 application and OT execution
 
 from DFS import DFS
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl
+from OTHandler import OTHandler
+from http.server import HTTPServer
+from SocketServer import ThreadingMixIn
+import ssl, threading
+
+class ThreadedHTTPSServer(ThreadingMixIn, HTTPServer):
+    pass
+
+#Inizialization of DFS and creation of the automata required
+states=["0", "1"]
+automata=DFS(states)
+
+automata.add_transitions('a_less', source="0", destination="0")
+automata.add_transitions('a_plus', "0", "1")
+automata.add_transitions('s_less', "0", "0")
+automata.add_transitions('s_plus', "0", "1")
+automata.add_transitions('v_less', "0", "0")
+automata.add_transitions('v_plus', "0", "1")
+automata.add_transitions('a_plus', "1", "1")
+automata.add_transitions('a_less', "1", "1")
+automata.add_transitions('s_plus', "1", "1")
+automata.add_transitions('v_plus', "1", "1")
+automata.add_transitions('v_less', "1", "1")
+automata.add_transitions('s_plus', "1", "0")
 
 #Inizialization of HTTPS server
 server_address = ('localhost', 4443)
-httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+otHandler=OTHandler(automata)
+httpd =ThreadedHTTPSServer(server_address, otHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', ssl_version=ssl.PROTOCOL_TLS)
 httpd.serve_forever()
 
