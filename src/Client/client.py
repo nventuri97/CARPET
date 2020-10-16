5044407c0b387e79c6066053a357f56cfea0cc62
nventuri97 <n.venturi3@studenti.unipi.it>
2020-10-16 15:31:33 +0200

    Update on JSON structure to send encrypted data

--- src/Client/client.py
+++ src/Client/client.py
@@ -3,29 +3,44 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 from http.client import HTTPSConnection
-import json
-import hashlib, ssl
+from base64 import b64encode, b64decode
+import hashlib, ssl, json, random
+import numpy as np
 
 #Example trace
 trace='a(-)a(-)s(-)v(-)s(+)s(+)a(+)s(-)s(-)'
 
 #New AES chiper to encrypt the trace that have to send
-key='Sixteen byte key'
+key=''.join(chr(random.randint(0, 0xFF)) for i in range(16))
 key = hashlib.sha256(key.encode()).digest()
 iv = Random.new().read(AES.block_size)
 cipher=AES.new(key,AES.MODE_CBC, iv)
+
+def encrypt(s):
+    encryptedString = cipher.encrypt(s)
+    return b64encode(encryptedString)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 #Connection with server
 connection=HTTPSConnection('127.0.0.1', 4443, timeout=10)
-if ConnectionError :
-    print("Something went wrong, try later")
 
 #First GET request to start the OT run
 connection.request('GET', '/')
 
-#Server's response containing the blinded vector
+#Server's response containing the blinded vector and cardinality of states set
 response=connection.getresponse()
-vet=(response.read())
+
+data=(response.read())
+data=json.loads(data)
+
+Q_len=data["CardState"]
+vet=data["BlindVector"]
 r_b=vet[0]
+
+#Binary vector of Q_len lenght
+binv=np.zeros(Q_len)
+binv[r_b]=1
+ciphertext= encrypt(binv)
+data=json.dumps({"ChiperText": ciphertext.decode('ascii')})
+connection.request('GET', '/', data.encode())
